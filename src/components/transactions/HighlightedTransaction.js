5a540923180e349cc4e72e986c4741e5b26c218b
bertjee321 <b.kruiter.91@gmail.com>
2022-07-13 20:31:48 +0200

    Added transaction detail page

--- src/components/transactions/HighlightedTransaction.js
+++ src/components/transactions/HighlightedTransaction.js
@@ -1,3 +1,34 @@
-const HighlightedTransaction = () => {};
+import Card from "../UI/Card";
+import classes from "./css/HighlightedTransaction.module.css";
+
+const HighlightedTransaction = (props) => {
+  const { date, category, description, account, amount } = props.data;
+  const month = date.toLocaleString("en-EN", { month: "long" });
+  const day = date.toLocaleString("en-EN", { day: "2-digit" });
+  const year = date.getFullYear();
+
+  return (
+    <Card className={classes.item}>
+      <div className={classes.left}>
+        <p className={classes["item__amount"]}>€ {amount.toFixed(2)}</p>
+        <div className={classes.date}>
+          <p className={classes["date__month"]}>{month}</p>
+          <p className={classes["date__year"]}>{year}</p>
+          <p className={classes["date__day"]}>{day}</p>
+        </div>
+      </div>
+      <div className={classes["item__description"]}>
+        <h2>{category}</h2>
+
+        <h4>Description</h4>
+        <p>
+          {description ? description : "n/a"}
+        </p>
+        <h4>Account</h4>
+        <p>{account}</p>
+      </div>
+    </Card>
+  );
+};
 
 export default HighlightedTransaction;
--- src/pages/IncomeDetails.js
+++ src/pages/IncomeDetails.js
@@ -1,5 +1,49 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import LoadingSpinner from "../components/UI/loadingspinner/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleIncome } from "../lib/api";
+import HighlightedTransaction from "../components/transactions/HighlightedTransaction";
+
 const IncomeDetails = () => {
-  return <h1>Income Details Page</h1>;
+  const params = useParams();
+  const { incomeId } = params;
+
+  const {
+    sendRequest,
+    data: loadedIncome,
+    error,
+    status,
+  } = useHttp(getSingleIncome);
+
+  useEffect(() => {
+    sendRequest(incomeId);
+  }, [sendRequest, incomeId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedIncome) {
+    return <p className="centered">No income with ID "{incomeId}" Found.</p>;
+  }
+
+  return (
+    <div className="main">
+      <h1 style={{ margin: "0px" }}>Income Details</h1>
+      <p style={{ margin: "0px", marginTop: "5px" }}>Income ID: {incomeId}</p>
+
+      <HighlightedTransaction data={loadedIncome} />
+    </div>
+  );
 };
 
 export default IncomeDetails;
