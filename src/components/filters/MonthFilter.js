a42fea46b587dbcfa9c467a1d28136b37c3d893c
bertjee321 <b.kruiter.91@gmail.com>
2022-07-23 20:13:07 +0200

    UI and functionality changes

--- src/components/filters/MonthFilter.js
+++ src/components/filters/MonthFilter.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 import "./css/filter.css";
 
 const MonthFilter = (props) => {
+  const [monthAndYear, setMonthAndYear] = useState({
+    month: ("0" + (new Date().getMonth() + 1)).slice(-2),
+    year: new Date().getFullYear().toString(),
+  });
+
   const dropdownChangeHandler = (e) => {
+    setMonthAndYear({
+      month: ("0" + (new Date(e.target.value).getMonth() + 1)).slice(-2),
+      year: new Date(e.target.value).getFullYear().toString(),
+    });
     props.onChangeFilter(e.target.value);
   };
-
-  const year = new Date().getFullYear().toString();
-  const month = ("0" + (new Date().getMonth() + 1)).slice(-2);
 
   return (
     <div className="form">
@@ -15,6 +23,7 @@ const MonthFilter = (props) => {
         <input
           type="month"
           onChange={dropdownChangeHandler}
+          value={monthAndYear.year + "-" + monthAndYear.month}
         />
       </div>
     </div>
