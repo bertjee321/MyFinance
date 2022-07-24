6892d26ad71642b65b02cf394208ae15d859ef54
bertjee321 <b.kruiter.91@gmail.com>
2022-07-24 10:47:51 +0200

    change look + feel of details page

--- src/components/transactions/HighlightedTransaction.js
+++ src/components/transactions/HighlightedTransaction.js
@@ -7,65 +7,66 @@ const HighlightedTransaction = (props) => {
   const day = date.toLocaleString("en-EN", { day: "2-digit" });
   const year = date.getFullYear();
 
+  const content = [
+    {
+      tag: "Category",
+      data: category,
+    },
+    {
+      tag: "Date",
+      data: day + " " + month + ", " + year,
+    },
+    {
+      tag: "Amount",
+      data: "€ " + amount.toFixed(2),
+    },
+    {
+      tag: "From / To",
+      data: "Unknown", // This part is not added to application / database yet!
+    },
+    {
+      tag: "Account",
+      data: account + " (IBAN?)", // IBAN is not part of database yet
+    },
+    {
+      tag: "Transaction ID",
+      data: props.id,
+    },
+    {
+      tag: "Description",
+      data: description,
+    },
+  ];
+
   return (
     <Card className={classes.item}>
       <header className={classes.head}>
         <div className={classes["head--item"]}>
-          <h4>{category}</h4>
-          <p>{day} {month}</p>
+          <h4 style={{ margin: "0 0 0 1rem" }}>{category}</h4>
+          <p style={{ margin: "0 0 0 1rem" }}>
+            {day} {month}
+          </p>
         </div>
-        <div className={classes["head--item"]}>Select option (placeholder)</div>
+        <div className={classes["head--item"]}>Select (placeholder)</div>
         <div className={classes["head--item"]}>
-          <p>€{amount.toFixed(2)}</p>
+          <p style={{ margin: "0 1rem 0 0", textAlign: "right" }}>
+            €{amount.toFixed(2)}
+          </p>
         </div>
       </header>
-      <content className={classes.content}>
+      <div className={classes.content}>
         <div className={classes["content--graph"]}>Some graph PLACEHOLDER</div>
-        <div className={classes["content--info"]}>
-          <section>
-            <p className={classes["section--left"]}>
-              <b>Category</b>
-            </p>
-            <p className={classes["section--right"]}>{category}</p>
-          </section>
-          <section>
-            <p className={classes["section--left"]}>
-              <b>Date</b>
-            </p>
-            <p className={classes["section--right"]}>
-              {day} {month}, {year}{" "}
-            </p>
-          </section>
-          <section>
-            <p className={classes["section--left"]}>
-              <b>From / To</b>
-            </p>
-            <p className={classes["section--right"]}>Unknown</p>
-          </section>
-          <section>
-            <p className={classes["section--left"]}>
-              <b>Account</b>
-            </p>
-            <p className={classes["section--right"]}>{account}</p>
-          </section>
-          <section>
-            <p className={classes["section--left"]}>
-              <b>ID</b>
-            </p>
-            <p className={classes["section--right"]}>
-              {props.id}
-            </p>
-          </section>
-          <section>
-            <p className={classes["section--left"]}>
-              <b>Description</b>
-            </p>
-            <p className={classes["section--right"]}>
-              {description}
-            </p>
-          </section>
-        </div>
-      </content>
+        <ul className={classes["content--list"]}>
+          {content.map((item, index) => (
+            <li key={index}>
+              <p className={classes["section--left"]}>
+                <b>{item.tag}</b>
+              </p>
+              <p className={classes["section--right"]}>{item.data}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Card>
   );
 };
